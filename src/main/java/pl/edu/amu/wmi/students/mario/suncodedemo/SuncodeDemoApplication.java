920ab4437cd6a9659934c2a3e3dcd34d5766ac8a
Mariusz <Mariusz@DESKTOP-97BVQEB>
2018-08-21 18:44:11 +0200

    JSF 2.3 configured

--- src/main/java/pl/edu/amu/wmi/students/mario/suncodedemo/SuncodeDemoApplication.java
+++ src/main/java/pl/edu/amu/wmi/students/mario/suncodedemo/SuncodeDemoApplication.java
@@ -2,11 +2,30 @@ package pl.edu.amu.wmi.students.mario.suncodedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.ServletContextAware;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
 
 @SpringBootApplication
-public class SuncodeDemoApplication {
+public class SuncodeDemoApplication implements ServletContextAware {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SuncodeDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SuncodeDemoApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean<FacesServlet> servletRegistrationBean() {
+        ServletRegistrationBean<FacesServlet> servletRegistrationBean = new ServletRegistrationBean<>(
+                new FacesServlet(), "*.xhtml");
+        servletRegistrationBean.setLoadOnStartup(1);
+        return servletRegistrationBean;
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+    }
 }
